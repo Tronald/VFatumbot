791a6a8e8c1cab1e81d3e53899df146112610f92
Simon McCorkindale <simon@funx.jp>
2019-10-20 17:21:08 +0900

    Typo fix

--- BotLogic/Consts.cs
+++ BotLogic/Consts.cs
@@ -42,6 +42,6 @@
         public const int WATER_POINTS_SEARCH_MAX = 10;
 
         // TODO: move later when localization is implemented
-        public const string NO_LOCATION_SET_MSG = "You haven't set a location. Send your location from (hint: you can do so by tapping the 🌍/::/＋/📎 icon in your app), or type \"search <address>\", or send a [Google Maps URL](https://www.google.com/maps/@51.509865,-0.118092,13z). Don't forget you can type \"help\" for more info.";
+        public const string NO_LOCATION_SET_MSG = "You haven't set a location. Send your location from the app (hint: you can do so by tapping the 🌍/::/＋/📎 icon in your app), or type \"search <address>\", or send a [Google Maps URL](https://www.google.com/maps/@51.509865,-0.118092,13z). Don't forget you can type \"help\" for more info.";
     }
 }
