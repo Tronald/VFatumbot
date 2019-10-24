1b7d4cc763c57601e9208992feb1b6bd1f0309c6
Simon Nishi McCorkindale <simon@funx.jp>
2019-10-25 01:52:08 +0900

    Script rename

--- BotLogic/CardFactory.cs
+++ BotLogic/CardFactory.cs
@@ -5,8 +5,32 @@ using VFatumbot.BotLogic;
 
 namespace VFatumbot
 {
-    public static class ReplyFactory
+    public static class CardFactory
     {
+        public static IMessageActivity CreateGetLocationFromGoogleMapsReply()
+        {
+            var attachments = new List<Attachment>();
+            var reply = MessageFactory.Attachment(attachments);
+
+            var cardAction = new CardAction(ActionTypes.OpenUrl, "Open Google Maps App", value: "https://maps.google.com");
+
+            var buttons = new List<CardAction> {
+                cardAction
+            };
+
+            var heroCard = new HeroCard
+            {
+                Title = "Facebook's removing the Send Location button",
+                Text = "Maps→Longpress & drop pin→Long-press pin→Share to Messenger→Randonauts",
+                Tap = cardAction,
+                Buttons = buttons,
+            };
+
+            reply.Attachments.Add(heroCard.ToAttachment());
+
+            return reply;
+        }
+
         public static IMessageActivity CreateLocationCardsReply(double[] incoords, dynamic w3wResult = null)
         {
             var attachments = new List<Attachment>();
@@ -24,8 +48,10 @@ namespace VFatumbot
                 new CardImage("https://maps.googleapis.com/maps/api/staticmap?&markers=color:red%7Clabel:C%7C" + incoords[0] + "+" + incoords[1] + "&zoom=15&size=" + Consts.THUMBNAIL_SIZE + "&maptype=roadmap&key=" + Consts.GOOGLE_MAPS_API_KEY),
             };
 
+            var cardAction = new CardAction(ActionTypes.OpenUrl, "Open", value: "https://www.google.com/maps/place/" + incoords[0] + "+" + incoords[1] + "/@" + incoords[0] + "+" + incoords[1] + ",18z");
+
             var buttons = new List<CardAction> {
-                new CardAction(ActionTypes.OpenUrl, "Open", value: "https://www.google.com/maps/place/" + incoords[0] + "+" + incoords[1] + "/@" + incoords[0] + "+" + incoords[1] + ",18z"),
+                cardAction
             };
 
             var heroCard = new HeroCard
@@ -33,7 +59,8 @@ namespace VFatumbot
                 Title = "Google Map",
                 Text = w3wResult != null ? ("What 3 Words address: " + w3wResult.words) : null,
                 Images = images,
-                Buttons = buttons
+                Buttons = buttons,
+                Tap = cardAction
             };
 
             return heroCard.ToAttachment();
@@ -45,8 +72,10 @@ namespace VFatumbot
                 new CardImage("https://maps.googleapis.com/maps/api/streetview?size=" + Consts.THUMBNAIL_SIZE + "&location=" + incoords[0] + "," + incoords[1] + "&fov=90&heading=235&pitch=10&key=" + Consts.GOOGLE_MAPS_API_KEY),
             };
 
+            var cardAction = new CardAction(ActionTypes.OpenUrl, "Open", value: "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=" + incoords[0] + "," + incoords[1] + "&fov=90&heading=235&pitch=10");
+
             var buttons = new List<CardAction> {
-                new CardAction(ActionTypes.OpenUrl, "Open", value: "https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=" + incoords[0] + "," + incoords[1] + "&fov=90&heading=235&pitch=10"),
+                cardAction
             };
 
             var heroCard = new HeroCard
@@ -54,7 +83,8 @@ namespace VFatumbot
                 Title = "Google Street View",
                 Text = "<TODO?>",
                 Images = images,
-                Buttons = buttons
+                Buttons = buttons,
+                Tap = cardAction,
             };
 
             return heroCard.ToAttachment();
@@ -66,8 +96,9 @@ namespace VFatumbot
                 new CardImage("https://maps.googleapis.com/maps/api/staticmap?&markers=color:red%7Clabel:C%7C" + incoords[0] + "+" + incoords[1] + "&zoom=18&size=" + Consts.THUMBNAIL_SIZE + "&maptype=satellite&key=" + Consts.GOOGLE_MAPS_API_KEY),
             };
 
+            var cardAction = new CardAction(ActionTypes.OpenUrl, "Open", value: "https://earth.google.com/web/@" + incoords[0] + "," + incoords[1] + ",146.726a,666.616d,35y,0h,45t,0r");
+
             var buttons = new List<CardAction> {
-                new CardAction(ActionTypes.OpenUrl, "Open", value: "https://earth.google.com/web/@" + incoords[0] + "," + incoords[1] + ",146.726a,666.616d,35y,0h,45t,0r")
             };
 
             var heroCard = new HeroCard
@@ -75,7 +106,8 @@ namespace VFatumbot
                 Title = "Google Earth View",
                 Text = incoords[0] + "," + incoords[1],
                 Images = images,
-                Buttons = buttons
+                Buttons = buttons,
+                Tap = cardAction
             };
 
             return heroCard.ToAttachment();
