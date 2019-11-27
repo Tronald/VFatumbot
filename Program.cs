fa5b69e265576e90658a70febf49b4474930bce2
Simon McCorkindale <simon@funx.jp>
2019-11-28 01:50:31 +0900

    Keep dev bot on Discord for now

--- Program.cs
+++ Program.cs
@@ -10,13 +10,13 @@ namespace VFatumbot
     {
         public static void Main(string[] args)
         {
-#if RELEASE_PROD // no dev version yet
+//#if RELEASE_PROD // no dev version yet
             // Run Discord bot
             DispatchWorkerThread((object sender, DoWorkEventArgs e) =>
             {
                 new DiscordBot().RunBotAsync().GetAwaiter().GetResult();
             });
-#endif
+//#endif
 
             // Run all other bots on the Bot Framework
             CreateWebHostBuilder(args).Build().Run();
