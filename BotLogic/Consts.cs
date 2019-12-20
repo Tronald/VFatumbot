35c71d588346e7331acbc79e6fdc4dc44a7e36c2
Simon McCorkindale <simon@funx.jp>
2019-12-21 00:06:35 +0900

    Bump prod version to 3.2.2

--- BotLogic/Consts.cs
+++ BotLogic/Consts.cs
@@ -3,7 +3,7 @@
     public class Consts
     {
 #if RELEASE_PROD
-        public const string APP_VERSION = "3.2.1";
+        public const string APP_VERSION = "3.2.2";
 #else
         public const string APP_VERSION = "3.3.0β";
 #endif
